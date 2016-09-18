8c03e6182e23882175d2eaf3c121f5cefaa09107
dessHub <deskip95@gmail.com>
2016-09-18 21:46:30 +0300

    second commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,42 @@ module.exports = function(app){
 
 app.get('/', function(req, res){
   res.render('pages/index.ejs')
-})
+});
+
+app.get('/vegetables', function(req, res){
+  res.render('pages/vegetables.ejs');
+});
+
+app.get('/fruits', function(req, res){
+  res.render('pages/fruits.ejs');
+});
+
+app.get('/kitchen', function(req, res){
+  res.render('pages/kitchen.ejs');
+});
+
+app.get('/staples', function(req, res){
+  res.render('pages/staples.ejs');
+});
+
+app.get('product', function(req, res){
+  res.render('pages/product.ejs');
+});
+
+app.get('/pcare', function(req, res){
+  res.render('pages/pcare.ejs');
+});
+
+app.get('/login', function(req, res){
+  res.render('pages/login.ejs');
+});
+
+app.get('/register', function(req, res){
+  res.render('pages/register.ejs');
+});
+
+app.get('/contact', function(req, res){
+  res.render('pages/contact.ejs');
+});
 
 };
